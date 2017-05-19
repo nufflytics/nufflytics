f423bfc7899005e3502f70a903f009debdd7e29d
Stephen Pearce <spearce@me.com>
2017-05-19 20:31:46 +1000

    added theme_nufflytics

---
+++ R/theme_nufflytics.R
@@ -0,0 +1,7 @@
+theme_nufflytics <- function(...) {
+  ggplot2::theme_linedraw(base_family = "Open Sans Condensed Bold") +
+    ggplot2::theme(plot.subtitle = ggplot2::element_text(family = "Open Sans Condensed Light"),
+          axis.text = ggplot2::element_text(family = "Open Sans Condensed Light"),
+          legend.text = ggplot2::element_text(family="Open Sans Condensed Light"),
+          legend.position = "bottom" )
+}
