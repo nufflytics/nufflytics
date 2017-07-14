088fcbb0a6c1ac35e114e9686d0d761962c0e1ed
Stephen Pearce <spearce@me.com>
2017-07-14 11:58:42 +1000

    export theme_nufflytics

--- R/theme_nufflytics.R
+++ R/theme_nufflytics.R
@@ -1,3 +1,10 @@
+#' Nufflytics theme
+#'
+#' @param ... extra parameters not currently used
+#'
+#' @return a modified \code{ggplot} theme for use on nufflytics.com
+#' @export
+#'
 theme_nufflytics <- function(...) {
   ggplot2::theme_linedraw(base_family = "Open Sans Condensed Bold") +
     ggplot2::theme(plot.subtitle = ggplot2::element_text(family = "Open Sans Condensed Light"),
